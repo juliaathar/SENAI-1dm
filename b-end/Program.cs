e0fdfd494beb00d223feebde65580078970eca9f
juliaathar <juathar@gmail.com>
2023-04-06 07:54:21 -0300

    start

---
+++ b-end/Program.cs
@@ -0,0 +1,21 @@
+﻿// string name = "Júlia";
+// Console.WriteLine (name);
+
+// int idade = 18;
+// Console.WriteLine("A idade da " + name + " é " + idade + " anos ");
+
+//crie um programa para calcular o IMC de uma pessoa
+
+//algoritmo
+
+string name = "Júlia";
+int idade = 18;
+float peso = 65;
+float altura = 1.65F;
+
+//processamento
+
+float imc = peso / (altura * altura);
+
+//saída 
+Console.WriteLine("O IMC da " + name + " é " + Math.Round(imc,2));
